9ce72133805b689936ecf1d822ee83b8fd23b471
Tanay Kumar <tanaykmrdev@gmail.com>
2024-03-16 17:36:51 +0530

    streamlined routing process

--- e-bye/src/App.jsx
+++ e-bye/src/App.jsx
@@ -1,22 +1,21 @@
-
-import './App.css'
-import Home from './components/Home'
-import Navbar from './components/Navbar'
-import Features from './components/Features'
-import About from './components/About'
-import ContactUs from './components/ContactUs'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
 
 function App() {
-
   return (
-    <>
-    <Navbar/>
-    <Home/>  
-    <Features/>
-    <About/>
-    <ContactUs/>
-    </>
-  )
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- e-bye/src/main.jsx
+++ e-bye/src/main.jsx
@@ -2,27 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import SellPage from './components/Sell/SellPage.jsx';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<App/>,
-  },
-  {
-    path: "btn1",
-    element:<SellPage/>,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
