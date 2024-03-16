6657924492f3aa3b72be30926666d166c874bbf9
Tanay Kumar <tanaykmrdev@gmail.com>
2024-03-16 17:11:10 +0530

    changed name of folder to user

--- e-bye/src/components/User/SellPage.jsx
+++ e-bye/src/components/User/SellPage.jsx
@@ -5,7 +5,7 @@ const SellPage = () => {
   return (
     <div>
       <Navbar />
-      <div className="container mx-auto" id='sell'>
+      <div className="container mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
           <Product
             image="product1.jpg"
